7782eb48bd9656a9a32033e78f90893cc96cd334
DavidAdolfoGomezUribe <96323893+DavidAdolfoGomezUribe@users.noreply.github.com>
2025-03-05 22:06:41 -0500

    feat: :sparkles: Enhance dashboard layout with new header and main container styles, add SVG icons, and implement menu toggle functionality

--- js/dashboard.js
+++ js/dashboard.js
@@ -4,10 +4,14 @@ console.log("its working")
 document.addEventListener("DOMContentLoaded", function() {
     const menuToggle = document.getElementById("menuToggle");
     const sidebar = document.getElementById("sidebar");
+    const headercontainer = document.querySelector(".headercontainer");
+    const maincontainer = document.querySelector(".maincontainer");
 
     menuToggle.addEventListener("click", function() {
         sidebar.classList.toggle("show"); // Agrega o quita la clase "show"
         menuToggle.classList.toggle("move"); // Mueve el botón
+        headercontainer.classList.toggle("expand")
+        maincontainer.classList.toggle("mainexpand")
         
     });
 });
