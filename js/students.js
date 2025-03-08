188ded60493443d1105d2a65f549b4489b0a0520
DavidAdolfoGomezUribe <96323893+DavidAdolfoGomezUribe@users.noreply.github.com>
2025-03-08 08:47:48 -0500

    feat: :sparkles: add initial HTML structure, CSS styles, and JavaScript for students page

---
+++ js/students.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", async function() {
+
+    try {
+
+        const maincontainertwo = document.querySelector("maincontainertwo");
+        
+        
+    } catch (error) {
+        
+    }
+    
+} )
